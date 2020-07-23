03cd4189aa1667378d00c77e48572725fb1bc2a6
prashant kumar sahu <prashantkumarsahu@gmail.com>
2020-07-23 18:02:15 +0530

    test re commit

--- reactjs/src/Constants.js
+++ reactjs/src/Constants.js
@@ -2,5 +2,5 @@ export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 export const USER_ROLE = 'USER_ROLE'
 
 export const API_URL = 'http://localhost:8100'
-
+//test2
 export const SCHOOL_NAME = 'Little World'
